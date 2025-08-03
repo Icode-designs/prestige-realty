c9fd90583caa76b07388fdf1afc3f72200921eb0
Osakwe Bonaventure <osakwebonaventure9@gmail.com>
2025-08-03 22:07:28 +0200

    9th commit

--- src/components/Findus.jsx
+++ src/components/Findus.jsx
@@ -3,16 +3,27 @@ import { FindusBox } from "@/styles/Findus.styles";
 import L from "leaflet";
 import { FlexBox, Heading2, SectionWrapper } from "@/styles/Ui.styles";
 import "leaflet/dist/leaflet.css";
+import { FaLocationDot } from "react-icons/fa6";
+import ReactDOMServer from "react-dom/server";
 
 const Findus = () => {
   useEffect(() => {
+    const iconHTML = ReactDOMServer.renderToString(
+      <FaLocationDot size={32} color="var(--col-30)" />
+    );
+
+    const parkIcon = L.divIcon({
+      html: iconHTML,
+      iconSize: [32, 32],
+      className: "custom-leaflet-icon", // prevent Leaflet's default styles
+    });
     const map = L.map("map", {
       center: [6.4285, 3.4442],
       zoom: 15,
       scrollWheelZoom: false, // disables zoom with mouse scroll
     });
 
-    L.marker([6.4285, 3.4442]).addTo(map);
+    L.marker([6.4285, 3.4442], { icon: parkIcon }).addTo(map);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
